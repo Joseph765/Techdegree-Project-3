9ba041b3bcfa8e9ac5adb63d1b4a7c5f182b187e
Joseph Chauvin <joseph.chauvin102@gmail.com>
2019-07-14 13:39:58 -0500

    Added jQuery and Normalize.css. Added focus on text input when page loads

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//focus on first input when page reloads
+$('#name').focus();
+
+//Job role 'other' input hiding
+// $('input#other-title').hide();
+
+//Job role 'other' showing when clicking
+// $('option[value="other"]').on();
