6c98d733fdf4dd192535fc2010b32a255869db49
Joseph Chauvin <joseph.chauvin102@gmail.com>
2019-07-18 21:18:16 -0500

    Refactoring Code 1

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 //focus on first input when page reloads
 $('#name').focus();
 
+
 //when the page loads, the 'creditcard' option is already selected
 $('#payment').val('credit card');
 $('#payPal').hide();
 $('#bitcoin').hide();
 
-//Job role 'other' input hiding
+
+//Job role 'other' option selected
 $('#other-title').hide();
 
 
@@ -20,15 +22,18 @@ $('#title').on('change', (e) => {
   }
 });
 
+
+//creating a counter for total price checked in checkboxes
+let totalPrice = 0;
+$('fieldset.activities').append('<p>Total Price: $' + totalPrice + '</p>');
+
+
 //function for hidding and disabling checkboxes
 function toggleCheckbox (checkbox, boolean, opacity) {
   $(checkbox).prop("disabled", boolean);
   $(checkbox).parent().css('opacity', opacity);
 }
 
-//creating a counter for total price checked in checkboxes
-let totalPrice = 0;
-$('fieldset.activities').append('<p>Total Price: $' + totalPrice + '</p>');
 
 //function for updating prices
 function updatePrice(operator, amount) {
@@ -41,16 +46,8 @@ function updatePrice(operator, amount) {
   }
 }
 
-function isValidEmail(email) {
-  if (/^[^@.]+@[^@.]+\.[a-z]+$/i.test(email)) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
-//each method: $('fieldset.activities input').each( function(element) {
-
+//function for checking if there are any checkboxes check in the 'activities' section
 function checkboxChecked() {
   const checkboxes = $('fieldset.activities input');
   for (i = 0; i < checkboxes.length; i += 1) {
@@ -63,7 +60,17 @@ function checkboxChecked() {
 }
 
 
-//validation for creditcard Number
+//function for validating email
+function isValidEmail(email) {
+  if (/^[^@.]+@[^@.]+\.[a-z]+$/i.test(email)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+//function for validating credit card number
 function isValidCcNumber(number){
   if (/\d{13}/.test(number) || /\d{14}/.test(number) || /\d{15}/.test(number) || /\d{16}/.test(number)) {
     return true;
@@ -74,6 +81,8 @@ function isValidCcNumber(number){
   }
 }
 
+
+//function for validating zip code number
 function isValidZipNumber(number){
   if (/\d{5}/.test(number)) {
     return true;
@@ -82,6 +91,8 @@ function isValidZipNumber(number){
   }
 }
 
+
+//function for validating CVV number
 function isValidCvv(number) {
   if (/\d{3}/.test(number)) {
     return true;
@@ -90,7 +101,34 @@ function isValidCvv(number) {
   }
 }
 
-//if any checkbox is checked, the alert paragraph: "At least one checkbox needs to be checked." will be removed
+
+//function for 'Theme - JS Puns' selected in design dropdown
+function jsPunsConfig(){
+  $('option[value="tomato"]').hide();
+  $('option[value="steelblue"]').hide();
+  $('option[value="dimgrey"]').hide();
+  $('option[value="cornflowerblue"]').show();
+  $('option[value="darkslategrey"]').show();
+  $('option[value="gold"]').show();
+  $("#color").val('cornflowerblue');
+  $('#colors-js-puns').show(); //unhidding color dropdown
+}
+
+
+//function for 'Theme - I ♥ JS' selected in design dropdown
+function iLoveJs() {
+  $('option[value="cornflowerblue"]').hide();
+  $('option[value="darkslategrey"]').hide();
+  $('option[value="gold"]').hide();
+  $('option[value="tomato"]').show();
+  $('option[value="steelblue"]').show();
+  $('option[value="dimgrey"]').show();
+  $("#color").val('tomato');
+  $('#colors-js-puns').show(); //unhidding color dropdown
+}
+
+
+//if a checkbox is checked, the alert paragraph: "At least one checkbox needs to be checked." will be removed
 $('fieldset.activities input').on('click', (e) => {
   const checkboxes = $('fieldset.activities input');
   for (i = 0; i < checkboxes.length; i += 1) {
@@ -101,35 +139,29 @@ $('fieldset.activities input').on('click', (e) => {
   }
 });
 
+
 //update T-shirt drop down when there are conflicting commands
 $('#colors-js-puns').hide(); //hiding T-shirt color before design is selected
 $('#design').on('change', () => {
   let selectedOption = $('#design option:selected').text();
   if (selectedOption === 'Theme - JS Puns') {
-    $('option[value="tomato"]').hide();
-    $('option[value="steelblue"]').hide();
-    $('option[value="dimgrey"]').hide();
-    $('option[value="cornflowerblue"]').show();
-    $('option[value="darkslategrey"]').show();
-    $('option[value="gold"]').show();
-    $("#color").val('cornflowerblue');
-    $('#colors-js-puns').show(); //unhidding color dropdown
+    jsPunsConfig();
   } else if (selectedOption === 'Theme - I ♥ JS') {
-    let tomato = $('option[value="tomato"]');
-    $('option[value="cornflowerblue"]').hide();
-    $('option[value="darkslategrey"]').hide();
-    $('option[value="gold"]').hide();
-    $('option[value="tomato"]').show();
-    $('option[value="steelblue"]').show();
-    $('option[value="dimgrey"]').show();
-    $("#color").val('tomato');
-    $('#colors-js-puns').show(); //unhidding color dropdown
+    iLoveJs();
   } else {
     $('#colors-js-puns').hide(); //hidding dropdown
   }
 });
 
+
+
+
+/////////////////////////////////////////////////////////////////////////
 //'Register for Activities' section. No conflicting schedules with jquery
+////////////////////////////////////////////////////////////////////////
+
+
+//main conference checkbox price updatings
 $('input[name="all"]').on('click', () => {
   if ($('input[name="all"]').is(':checked')) {
     updatePrice('+', 200);
@@ -138,6 +170,8 @@ $('input[name="all"]').on('click', () => {
   }
 });
 
+
+//Javascript framworks checkbox price updates, and disabling express checkbox
 $('input[name="js-frameworks"]').on('click', () => {
   if ($('input[name="js-frameworks"]').is(':checked')) {
     toggleCheckbox('input[name="express"]', true, 0.5);
@@ -148,6 +182,8 @@ $('input[name="js-frameworks"]').on('click', () => {
   }
 });
 
+
+//JavaScript libraries checkbox price updates, and disabling Node checkbox
 $('input[name="js-libs"]').on('click', () => {
   if ($('input[name="js-libs"]').is(':checked')) {
     toggleCheckbox('input[name="node"]', true, 0.5);
@@ -158,6 +194,8 @@ $('input[name="js-libs"]').on('click', () => {
   }
 });
 
+
+//Express checkbox price updates and disabling JavaScript Frameworks checkbox
 $('input[name="express').on('click', () => {
   if ($('input[name="express"]').is(':checked')) {
     toggleCheckbox('input[name="js-frameworks"]', true, 0.5);
@@ -168,6 +206,8 @@ $('input[name="express').on('click', () => {
   }
 });
 
+
+//Node checkbox price updates, and disabling JavaScript Libraries checkbox
 $('input[name="node"]').on('click', () => {
   if ($('input[name="node"]').is(':checked')) {
     toggleCheckbox('input[name="js-libs"]', true, 0.5);
@@ -178,6 +218,8 @@ $('input[name="node"]').on('click', () => {
   }
 });
 
+
+//Build tools checkbox price updates
 $('input[name="build-tools"]').on('click', () => {
   if ($('input[name="build-tools"]').is(':checked')) {
     updatePrice('+', 100);
@@ -186,6 +228,8 @@ $('input[name="build-tools"]').on('click', () => {
   }
 });
 
+
+//npm checkbox price updates
 $('input[name="npm"]').on('click', () => {
   if ($('input[name="npm"]').is(':checked')) {
     updatePrice('+', 100);
@@ -193,6 +237,7 @@ $('input[name="npm"]').on('click', () => {
     updatePrice('-', 100);
   }
 });
+
 
 //Updating based on form of payment selected
 $('#payment').on('change', () => {
@@ -216,6 +261,8 @@ $('#payment').on('change', () => {
   }
 });
 
+
+//automatically update valid email adress indicator
 $('input#mail').on('keyup', () => {
   if (isValidEmail($('input#mail').val()) === false || $('input#mail').val() == '') {
     $('input#mail').css('border-color', 'red');
@@ -224,69 +271,103 @@ $('input#mail').on('keyup', () => {
   }
 });
 
-//form validation
+
+/////////////////////////////////////////////////////////////////////////
+////////////////////////Form Validation/////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+
+//starts validation by listening for submit button clicked
 $('button[type="submit"]').on('click', (e) => {
   let selectedOption = $('#payment option:selected').text();
-  if ($('input#name').val() == '') { //if name is empty
+
+
+  //Adds error indication for invalid name and prevents form from being submitted
+  if ($('input#name').val() == '') {
     $('#name').css('border-color', 'red');
     e.preventDefault();
   }
 
-  //if is valid email adress (formated like one)
+
+  //if email is not valid email adress (formated like one), then form is not submitted and error indication
   if (isValidEmail($('input#mail').val()) === false || $('input#mail').val() == '') {
     $('input#mail').css('border-color', 'red');
     e.preventDefault();
   }
 
-  //at least one checkbox is checked
-  if ($('p.alert')) {
-    $('p.alert').remove();
+
+  //remove checkbox alert before a new one is created
+  if ($('p.checkbox-alert')) {
+    $('p.checkbox-alert').remove();
   }
+
+
+  //validation if no checkboxes are checked and prevents form from being submitted
   if (checkboxChecked() === false) {
-    $('fieldset.activities').prepend('<p class="alert">At least one checkbox needs to be checked.</p>')
-    $('p.alert').css('color', 'red');
+    $('fieldset.activities').prepend('<p class="checkbox-alert">At least one checkbox needs to be checked.</p>')
+    $('p.checkbox-alert').css('color', 'red');
     e.preventDefault();
   } else {
-    $('p.alert').remove();
+    $('p.checkbox-alert').remove();
   }
 
-  //form validation for creditcard info
+
+  //form validation for creditcard info, only if credit card is selected in dropdown
   if (selectedOption == 'Credit Card') {
 
-    if ($('p.blankCC')) { //removes 'blank' error if it exists already
-      $('p.blankCC').remove();
+
+    //removes alert for empty credit card form before adding a new one
+    if ($('p.blankCCAlert')) {
+      $('p.blankCCAlert').remove();
     }
 
+
+    //Validation for any blank credit card forms and prevents form from being submitted
     if ($('#cc-num').val() == '' || $('#zip').val() == '' || $('cvv').val()) {
       e.preventDefault();
-      $('#credit-card').prepend('<p class="blankCC">One or more Credit Card forms are empty</p>');
-      $('p.blankCC').css('color', 'red');
-    } else {
+      $('#credit-card').prepend('<p class="blankCCAlert">One or more Credit Card forms are empty</p>');
+      $('p.blankCCAlert').css('color', 'red');
+    }
 
+    //If the forms aren't blank, these next lines will validate if other information is correct
+    else {
+
+
+      //Removes Credit Card Number validation alert before the code creates a new one
       if ($('p.ccDigits')) {
         $('p.ccDigits').remove();
       }
 
+
+      //adds error message for credit card number form validation and prevents form from being submitted
       if (isValidCcNumber($('input#cc-num').val()) === false) {
         e.preventDefault();
         $('#credit-card').prepend('<p class="ccDigits">Please enter a credit card number that is between 13 and 16 digits long');
         $('p.ccDigits').css('color', 'red');
       }
 
+
+      //Removes zip code error message before the code creates a new one
       if($('p.zipError')) {
         $('p.zipError').remove();
       }
 
+
+      //adds error message for zip code form validation and prevents form from being submitted
       if(isValidZipNumber($('#zip').val()) === false) {
         e.preventDefault();
         $('#credit-card').prepend('<p class="zipError">A zip code MUST be 5 digits long.</p>');
         $('p.zipError').css('color', 'red');
       }
 
+
+      //Removes CVV error message before the code creates a new one
       if($('p.cvvError')) {
         $('p.cvvError').remove();
       }
 
+
+      //adds error message for invalid CVV number and prevents form from being submitted
       if(isValidCvv($('#cvv').val()) === false ) {
         e.preventDefault();
         $('#credit-card').prepend('<p class="cvvError">A CVV must be three digits long</p>');
